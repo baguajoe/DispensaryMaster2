5af779c8357eea805de819614c68e42843c96173
baguajoe <baguajoe@gmail.com>
2024-12-03 22:32:31 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,9 +1,10 @@
 // src/Layout.js
 import React from 'react';
-import { Outlet, Link, useNavigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import "../../styles/navbar.css"
 
 const Navbar = () => {
-  const navigate = useNavigate();
+  const laoction = useLocation();
 
   const handleSignOut = () => {
     // Clear user session (e.g., remove token from localStorage)
@@ -11,32 +12,55 @@ const Navbar = () => {
     navigate('/login'); // Redirect to login page
   };
 
+  const isActive = (path) => {
+    return location.pathname === path;
+  };
+
   return (
     <div>
       {/* Navbar */}
-      <nav class="navbar navbar-light navbar navbar-expand-lg" style={{backgroundColor: "#e3f2fd"}}>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className='nav-item'><Link className='nav-link' to="/">Home</Link></li>
-          <li className='nav-item'><Link className='nav-link' to="/dashboard">Dashboard</Link></li>
-          <li className="nav-item"><Link className='nav-link' to="/products">Products</Link></li>
-          <li className='nav-item'><Link className='nav-link' to="/inventory">Inventory</Link></li>
-          <li className='nav-item'><Link className='nav-link' to="/invoices">Invoices</Link></li>
-          <li className='nav-item'><Link className='nav-link' to="/customers">Customers</Link></li>
-          <li className='nav-item'><Link className='nav-link' to="/analytics">Analytics</Link></li>
-          <li className='nav-item'><Link className='nav-link' to="/register">Register</Link></li>
-          <li className='nav-item'><button className='nav-link' onClck={handleSignOut} style={{ background: 'none', border: 'none', color: 'blue', cursor: 'pointer' }}>Sign Out</button></li>
-        </ul>
+      <nav className="navbar navbar-expand-lg bg-body-tertiary">
+        <div className="container-fluid mx-5">
+          <a className="navbar-brand" href="#">Logo</a>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"><i className="fa-solid fa-bars"></i></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul className="navbar-nav ms-auto">
+              <li className="nav-item">
+                <a className={`nav-link ${isActive('/') ? 'active' : ''}`} aria-current="page" href="/">Home</a>
+              </li>
+              <li className="nav-item">
+                <a className={`nav-link ${isActive('/register') ? 'active' : ''}`} href="/register">
+                  Register
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className={`nav-link ${isActive('/login') ? 'active' : ''}`} href="#">Login</a>
+              </li>
+              <li className="nav-item">
+                <a className={`nav-link ${isActive('/logout') ? 'active' : ''}`} href="#" onClick={(e) => {
+                    e.preventDefault();
+                    handleSignOut();
+                  }}>Logout</a>
+              </li>
+              <li className="nav-item dropdown">
+                <a className={`nav-link dropdown-toggle ${isActive('/change-password') || isActive('/profile') ? 'active' : ''}`} href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  Settings
+                </a>
+                <ul className="dropdown-menu">
+                  <li><a className="dropdown-item" href="#">light dark mode</a></li>
+                  <li><a className="dropdown-item" href="#">Change Password</a></li>
+                  <li><a className="dropdown-item" href="#">update account/profile </a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
 
-      {/* Main content area */}
-     
-      {/* Footer */}
-     
+
+
     </div>
   );
 };
