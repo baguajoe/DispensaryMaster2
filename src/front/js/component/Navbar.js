38e6da73634e7d278a0d6deccbd7bcb325d133a6
baguajoe <baguajoe@gmail.com>
2026-03-31 16:43:22 +0000

    Fix: remove nonexistent BudphoriaPro.png import from Navbar

--- src/front/js/component/Navbar.js
+++ src/front/js/component/Navbar.js
@@ -2,7 +2,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import "../../styles/navbar.css"
-import logo from "../../../../docs/assets/BudphoriaPro.png"
 
 const Navbar = () => {
   const navigate = useNavigate();
